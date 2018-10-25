693eb0e24ccf3f11612ae4aaea8596afc8583a89
R. Torsten Clay <rt_clay@bellsouth.net>
2018-10-25 03:10:10 +0000

    Fix Stew Perry exchange entry: take last grid on line

--- so2sdr/contest_stew.cpp
+++ so2sdr/contest_stew.cpp
@@ -131,16 +131,17 @@ bool Stew::validateExchange(Qso *qso)
     if (!separateExchange(qso)) return(false);
 
     bool ok = true;
+    const int idx = exchElement.size()-1;
     qso->bandColumn=qso->band;
 
-    // check to see if this is a valid grid square
-    if (exchElement.at(0).size() != 4 || exchElement.at(0).at(0) < 'A' || exchElement.at(0).at(0) > 'R' ||
-        exchElement.at(0).at(1) < 'A' || exchElement.at(0).at(1) > 'R' ||
-        exchElement.at(0).at(2) < '0' || exchElement.at(0).at(2) > '9' ||
-        exchElement.at(0).at(3) < '0' || exchElement.at(0).at(3) > '9') {
+    // check to see if the last entered is a valid grid square
+    if (exchElement.at(idx).size() != 4 || exchElement.at(idx).at(0) < 'A' || exchElement.at(idx).at(0) > 'R' ||
+        exchElement.at(idx).at(1) < 'A' || exchElement.at(idx).at(1) > 'R' ||
+        exchElement.at(idx).at(2) < '0' || exchElement.at(idx).at(2) > '9' ||
+        exchElement.at(idx).at(3) < '0' || exchElement.at(idx).at(3) > '9') {
         ok = false;
     } else {
-        finalExch[0]     = exchElement[0];
+        finalExch[0]     = exchElement[idx];
         qso->rcv_exch[0] = finalExch[0];
     }
     return(ok);
